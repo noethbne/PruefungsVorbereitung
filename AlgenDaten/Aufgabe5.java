3f5fbbdab7f72b0b5d1b79a7d7fe8c74bb419b92
noethbne <noeth.benedikt@gmail.com>
2022-06-04 11:51:52 +0200

    sdafa

---
+++ AlgenDaten/Aufgabe5.java
@@ -0,0 +1,17 @@
+package AlgenDaten;
+public class Aufgabe5 {
+
+    public int [] getMinMax(int array[])
+    {
+        int max = array[0];
+        int min = array[0];
+        for (int i=1; i<array.length; i++)
+        {
+            if (array[i] > max) max = array[i];
+            else if (array[i] < min) min = array[i];
+        }
+
+        int res[] = {min, max};
+        return res;
+    }
+}
