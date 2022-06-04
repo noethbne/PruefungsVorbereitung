060503d9834067f87a60a66cbbab1b553fbba6e6
noethbne <noeth.benedikt@gmail.com>
2022-06-04 11:44:32 +0200

    Aufgabe 43

--- AlgenDaten/Aufgabe4.java
+++ AlgenDaten/Aufgabe4.java
@@ -4,17 +4,19 @@ public class Aufgabe4 {
     public static void bucketSort(int array[])
     {
         int max = array[0];
+        int min = array[0];
         
         for (int i=1; i<array.length; i++)
         {
             if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i];
         }
         
-        int buckets[] = new int[max+1];
+        int buckets[] = new int[max - min + 1];
 
         for (int i=0; i<array.length; i += 1)
         {
-            buckets[array[i]] = buckets[array[i]] + 1;
+            buckets[array[i]-min] = buckets[array[i]-min] + 1;
         }
 
         int idx=0;
@@ -22,7 +24,7 @@ public class Aufgabe4 {
         {
             for (int i=0; i<buckets[b]; i += 1)
             {
-                array[idx] = b;
+                array[idx] = b + min;
                 idx += 1;
             }
         }
@@ -36,12 +38,12 @@ public class Aufgabe4 {
     }
 
     public static void main(String[] args){
-        int[] test1 = {4,2,8,7,5,0,3,9};
+        int[] test1 = {4,2,8,7,-5,0,3,9};
         int[] test2 = {10,9,8,7,6,5,4,3,2,1,0};
 
         print(test1);
         bucketSort(test1);
-        print(test1);
+        print(test1); 
         System.out.println();
         print(test2);
         bucketSort(test2);
